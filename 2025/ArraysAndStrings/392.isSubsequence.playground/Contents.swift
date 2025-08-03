340bbe136bb87cdafe4b7cf4efa19af4db7d9fd8
AlexKurbatov <AlexKurbatov@icloud.com>
2025-08-03 09:56:15 +0500

    Added solution 392.isSubsequence

---
+++ 2025/ArraysAndStrings/392.isSubsequence.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+func isSubsequence(_ subSequence: String, _ sequence: String) -> Bool {
+    
+    var sequenceIndex = sequence.startIndex
+    var subSequenceIndex = subSequence.startIndex
+    
+    while sequenceIndex < sequence.endIndex && subSequenceIndex < subSequence.endIndex {
+        if sequence[sequenceIndex] == subSequence[subSequenceIndex] {
+            sequenceIndex = sequence.index(after: sequenceIndex)
+            subSequenceIndex = subSequence.index(after: subSequenceIndex)
+        } else {
+            sequenceIndex = sequence.index(after: sequenceIndex)
+        }
+    }
+    
+    return subSequenceIndex == subSequence.endIndex
+}
+
+isSubsequence("ace", "acde")
